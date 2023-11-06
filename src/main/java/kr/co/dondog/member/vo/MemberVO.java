55036fa089441817ab4675ba973d3e4a3efe4085
조철민 <destiny5418@gmail.com>
2023-11-06 19:34:41 +0900

    1106-4 19:34 커밋

--- src/main/java/kr/co/dondog/member/vo/MemberVO.java
+++ src/main/java/kr/co/dondog/member/vo/MemberVO.java
@@ -1,32 +1,106 @@
 package kr.co.dondog.member.vo;
 
-import java.sql.Date;
+import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MemberVO {
-	
-	private String email; // member 식별자
-	private String mname;
-	private String pwd;
-	private String nickname;
-	private String address;
-	private String birth; 
-	private int gender; //1 여자 2 남자 체크박스
-	private int report; //신고누적수 default 0
-	
-	/* 관리자 */
-	private Date joinDate;
-	private Date recentLog; // 마지막 접속일
-	private String grade; // admin: A, generic: G default 'G'
 
-	private Date updateTime; // 수정일
-	private String oauth;
-	private String accountExpired; //default 'N'
-	private String accountLocked; //default 'N'
-	private int loginCount; //  default 0
-	private String cancle; // default 'N'
+   private String email;
+   private String mname;
+   private String pwd;
+   private String nickname;
+   private String address;
+   private String birth;
+   private LocalDateTime joinDate;
+   private String grade;
+   private String report;
+   private LocalDateTime recentLog;
+   private String gender;
+   private String oauth;
+   private String accountExpired;
+   private String accountLocked;
+   private int loginCount;
+   private LocalDateTime updateTime;
+   private String cancel;
 
+   @Builder
+   public MemberVO(String email, String pwd, String nickname, String address, String birth,
+         LocalDateTime joinDate, String grade, String report, LocalDateTime recentLog, String gender, String oauth,
+         String accountExpired, String accountLocked, int loginCount, LocalDateTime updateTime, String cancel) {
+      this.email = email;
+      this.pwd = pwd;
+      this.nickname = nickname;
+      this.address = address;
+      this.birth = birth;
+      this.joinDate = joinDate;
+      this.grade = grade;
+      this.report = report;
+      this.recentLog = recentLog;
+      this.gender = gender;
+      this.oauth = oauth;
+      this.accountExpired = accountExpired;
+      this.accountLocked = accountLocked;
+      this.loginCount = loginCount;
+      this.updateTime = updateTime;
+      this.cancel = cancel;
+   }
 
+//   @Override
+//   public String getPassword() {
+//      return this.getPwd();
+//   }
+//
+//   @Override
+//   public String getUsername() {
+//      return this.getEmail();
+//   }
+//
+//   // 계정이 갖고있는 권한 목록은 리턴
+//   @Override
+//   public Collection<? extends GrantedAuthority> getAuthorities() {
+//
+//      Collection<GrantedAuthority> collectors = new ArrayList<>();
+//      collectors.add(() -> {
+//         return "계정별 등록할 권한";
+//      });
+//
+//      // collectors.add(new SimpleGrantedAuthority("user"));
+//
+//      return collectors;
+//   }
+//
+////계정이 만료되지 않았는지 리턴 (true: 만료 안됨)
+//   @Override
+//   public boolean isAccountNonExpired() {
+//      return "N".equalsIgnoreCase(accountExpired);
+//      // return true;
+//   }
+//
+////계정이 잠겨있는지 않았는지 리턴. (true: 잠기지 않음)
+//   @Override
+//   public boolean isAccountNonLocked() {
+//      return "N".equalsIgnoreCase(accountLocked);
+//      // return true;
+//   }
+//
+////비밀번호가 만료되지 않았는지 리턴한다. (true: 만료 안됨)
+//   @Override
+//   public boolean isCredentialsNonExpired() {
+//      return true;
+//   }
+//
+////계정이 활성화(사용가능)인지 리턴 (true: 활성화)
+//   @Override
+//   public boolean isEnabled() {
+//      return true;
+//   }
 }
